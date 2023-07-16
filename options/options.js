c6819b5b7715050ce65fd49595ca8fd1b5869613
Crowdbotics <engineering@crowdbotics.com>
2023-07-16 23:23:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ancient-darkness-d-101928",
+  url: "https://ancient-darkness-d-101928.botics.co",
+  api: "https://ancient-darkness-d-101928.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
